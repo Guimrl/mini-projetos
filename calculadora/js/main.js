27ac108c7381de8dabcc7d666f104776519be4e2
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-14 12:28:44 -0300

    criando estilo/digitando numeros

--- calculadora/js/main.js
+++ calculadora/js/main.js
@@ -1,1 +1,67 @@
-alert('hello world!')
+const numbers = document.querySelectorAll(".num");
+const result = document.querySelector('.result span');
+const signs = document.querySelectorAll('.sign');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const negative = document.querySelector('.negative');
+const percent = document.querySelector('.percent');
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for(let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', (e) => {
+        let atr = e.target.getAttribute('value');
+        if(isFirstValue === false) {
+            getFirstValue(atr);
+        }
+
+        if(isSecondValue == false) {
+            getSecondValue(atr);
+        }
+    })
+}
+
+function getFirstValue(el) {
+    result.innerHTML = "";
+    firstValue += el;
+    result.innerHTML = firstValue;
+    firstValue = +firstValue;
+}
+
+function getSecondValue(el) {
+    if(firstValue != "" && sign != "") {
+        secondValue += el;
+        result.innerHTML = secondValue;
+        secondValue = +secondValue;
+    }
+}
+
+function getSign() {
+    for(let i = 0; i < signs.lenght; i++) {
+        signs[i].addEventListener('click', (e) => {
+            sign = e.target.getAttribute('value');
+            isFirstValue = true;
+        })
+    }
+}
+getSign();
+equals. addEventListener('click', () => {
+    result.innerHTML = "";
+    if(sign === "+") {
+        resultValue = firstValue + secondValue;
+    } else if(sign === "-") {
+        resultValue = firstValue - secondValue;
+    }else if(sign === "x") {
+        resultValue = firstValue * secondValue;
+    }else if(sign === "/") {
+        resultValue = firstValue / secondValue;
+    }
+    result.innerHTML = resultValue;
+    firstValue = resultValue;
+    secondValue = ""
+})
