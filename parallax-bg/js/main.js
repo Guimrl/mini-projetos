624dec77345984c39ef7fcfbbffe2b25ca2b4f44
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-21 19:21:23 -0300

    final commit

--- parallax-bg/js/main.js
+++ parallax-bg/js/main.js
@@ -1,1 +1,11 @@
-alert('hello world')
+const handleScroll = (event) => {
+    const scrollPosition = event.target.scrollingElement.scrollTop;
+    console.log("oi")
+    const images = document.querySelectorAll("img");
+    console.log("images", images);
+    images.forEach((element) => {
+        element.style.transform = `translate(0, ${scrollPosition / 10}px)`;
+    });
+}
+
+window.addEventListener("scroll", handleScroll);
