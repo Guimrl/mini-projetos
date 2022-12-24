626d82903209ec2afae8700474291572d41dbb4b
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-24 19:28:07 -0300

    criando card usuario

--- perfil-github/js/main.js
+++ perfil-github/js/main.js
@@ -1,6 +1,33 @@
 const url = 'https://api.github.com/users/';
+const form = document.getElementById('form');
+const pesquisar = document.getElementById('pesquisa');
 console.log(url)
-async function getUsuario() {
-    const data = await axios(url + 'guimrl')
-    console.log(data)
+async function getUsuario(nomeDeUsuario) {
+    try {
+        const { data } = await axios(url + nomeDeUsuario);
+
+    } catch(err) {
+        if(err.resposta.status == 404) {
+            erroCard('Usuário não encontrado');
+        }
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const usuario = pesquisar.value;
+    if(usuario) {
+        getUsuario(usuario);
+
+        pesquisar.value = '';
+    }
+})
+
+erroCard(msg) {
+    const cardHTML = `
+    <div class="card">
+        <h1>${msg}</h1>
+    </div>
+    `
 }
