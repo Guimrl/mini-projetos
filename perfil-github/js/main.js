e837f5b244b34ce891c716ec6fee3ce7a965ce96
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-24 20:16:27 -0300

    final commt

--- perfil-github/js/main.js
+++ perfil-github/js/main.js
@@ -1,85 +1,91 @@
-const url = 'https://api.github.com/users/';
-const form = document.getElementById('form');
-const pesquisar = document.getElementById('pesquisa');
+const APIURL = 'https://api.github.com/users/';
+
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const pesquisar = document.getElementById('pesquisar');
 
-async function getUsuario(nomeDeUsuario) {
+async function getUser(nomeDeUsuario) {
     try {
-        const { dados } = await axios(url + nomeDeUsuario);
+        const { data } = await axios(APIURL + nomeDeUsuario);
 
-        criaCardDeUsuario(dados);
-        getRepositorios(nomeDeUsuario);
-    } catch (err) {
-        if (err.resposta.status == 404) {
-            exibeErroNoCard('Usuário não encontrado');
+        createUserCard(data);
+        getRepos(nomeDeUsuario);
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('Usuário não encontrado');
         }
     }
 }
 
-async function getRepositorios(nomeDeUsuario) {
+async function getRepos(nomeDeUsuario) {
     try {
-        const { dados } = await axios(url + nomeDeUsuario + '/repos?sort=created');
+        const { data } = await axios(APIURL + nomeDeUsuario + '/repos?sort=created');
 
-        adicionaRepoAoCard(dados);
-    } catch (err) {
-        exibeErroNoCard('Problema ao buscar repositórios');
+        addReposToCard(data);
+    } catch(err) {
+        createErrorCard('Problema ao buscar repositório');
     }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const usuario = pesquisar.value;
-    if (usuario) {
-        getUsuario(usuario);
-
-        pesquisar.value = '';
-    }
-})
-
-function criaCardDeUsuario(usuario) {
-    const idUsuario = usuario.name || usuario.login;
-    const BioUsuario = usuario.bio ? `<p>${usuario.bio}</P` : ''
+function createUserCard(user) {
+    const userID = user.name || user.login;
+    const userBio = user.bio ? `<p>${user.bio}</p>` : '';
     const cardHTML = `
     <div class="card">
     <div>
-        <img src="${usuario.avatar_url}" class="img">
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="user-info">
-        <h2>${idUsuario}</h2>
-        ${BioUsuario}
-    <ul>
-        <li>${usuario.followers} <strong>Followers</strong></li>
-        <li>${usuario.following} <strong>Following</strong></li>
-        <li>${usuario.public_repos} <strong>Repos</strong></li>
-    </ul>
-    <div id="repositorio"></div>
-    </div>
-    </div>
-        `
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
 
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
     main.innerHTML = cardHTML;
+    
 }
 
-function adicionaRepoAoCard(repos) {
-    const reposEl = document.getElementById('repositorio');
-    repos.slice(0, 5)
-    .forEach(repo => {
-        const repoEl = document.createElement('a');
-        repoEl.classList.add('repositorio');
-        repoEl.href = repo.html_url;
-        repoEl.innerText = repo.name;
-
-        reposEl.appendChild(repoEl)
-    })
-}
-
-function exibeErroNoCard(msg) {
+function createErrorCard(msg) {
     const cardHTML = `
-    <div class="card">
-        <h1>${msg}</h1>
-    </div>
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
     `
 
     main.innerHTML = cardHTML;
 }
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos');
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a');
+            repoEl.classList.add('repo');
+            repoEl.href = repo.html_url;
+            repoEl.target = '_blank';
+            repoEl.innerText = repo.name;
+
+            reposEl.appendChild(repoEl);
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = pesquisar.value;
+
+    if(user) {
+        getUser(user);
+
+        pesquisar.value = '';
+    }
+})
