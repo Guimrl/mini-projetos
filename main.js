6d607193cd8faca498c20419a2daff749edced09
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-06 00:23:44 -0300

    separando arquivo de projetos

--- main.js
+++ main.js
@@ -1,30 +1,5 @@
 
-const projetos = [
-    {
-        title: "parallax",
-        url: "https://guimrl.github.io/mini-projetos/parallax/"
-    },
-    {
-        title: "Perfil Github",
-        url: "https://guimrl.github.io/mini-projetos/perfil-github/"
-    },
-    {
-        title: "Cronômetro",
-        url: "https://guimrl.github.io/mini-projetos/cronometro/"
-    },
-    {
-        title: "Calculadora",
-        url: "https://guimrl.github.io/mini-projetos/calculadora/"
-    },
-    {
-        title: "Mouse Tracker",
-        url: "https://guimrl.github.io/mini-projetos/mouse-tracker/"
-    },
-    {
-        title: "Bloco de Notas",
-        url: "https://guimrl.github.io/mini-projetos/bloco-de-notas/"
-    }
-];
+
 
 for (let i = 0; i < projetos.length; i++) {
     let section = document.querySelector('#conteudo');
---
+++ projetos.js
@@ -0,0 +1,28 @@
+
+const projetos = [
+    {
+        title: "parallax",
+        url: "https://guimrl.github.io/mini-projetos/parallax/"
+    },
+    {
+        title: "Perfil Github",
+        url: "https://guimrl.github.io/mini-projetos/perfil-github/"
+    },
+    {
+        title: "Cronômetro",
+        url: "https://guimrl.github.io/mini-projetos/cronometro/"
+    },
+    {
+        title: "Calculadora",
+        url: "https://guimrl.github.io/mini-projetos/calculadora/"
+    },
+    {
+        title: "Mouse Tracker",
+        url: "https://guimrl.github.io/mini-projetos/mouse-tracker/"
+    },
+    {
+        title: "Bloco de Notas",
+        url: "https://guimrl.github.io/mini-projetos/bloco-de-notas/"
+    }
+    
+];
