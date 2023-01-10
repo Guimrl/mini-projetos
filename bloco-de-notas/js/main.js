13a80580884f485836cf97321ad39c683159a137
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-10 10:24:58 -0300

    criando botão de remover

--- bloco-de-notas/js/main.js
+++ bloco-de-notas/js/main.js
@@ -33,12 +33,39 @@ function carregaBancoDeDados() {
 function novaNota(item) {
     const div = document.createElement('div');
 
-    div.innerHTML = `<div class="item" style="background-color: ${item?.color || corAleatoria()}">
+    div.innerHTML = `<div class="item" style="background-color: ${item?.cores || corAleatoria()}">
     <span class="remove">x</span>
     <textarea>${item?.text || ""}</textarea>
     </div>`
 
     conteudo.appendChild(div);
+}
+
+function eventos() {
+    const novaNota = document.querySelectorAll(".item textarea");
+    const remove = document.querySelectorAll(".item .remove");
+
+    novaNota.forEach((item, i) => {
+        item.oninput = () => {
+            notas[i] = {
+                text: item.ariaValueMax,
+                color: notas[i]?.cores || item.parentElement.style.backgroundColor
+            }
+
+            localStorage.setItem("notas", JSON.stringify(notas));
+        }
+    });
+
+    remove.forEach((item, i) => {
+        item.onclick = () => {
+            conteudo.children[i].remove();
+            if(notas[i]) {
+                notas.splice(i, 1);
+                localStorage.setItem("notas", JSON.stringify(notas));
+            }
+            eventos();
+        }
+    })
 }
 
 function verificaNulls() {
