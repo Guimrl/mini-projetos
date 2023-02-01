6eba11f4eb13abe4ce797e88fc8e84ade363c417
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-01 14:47:22 -0300

    ordenando os links por ordem alfabética

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const URL_PADRAO = "https://guimrl.github.io/mini-projetos/";
+projetos.sort((a, b) => a.title.localeCompare(b.title));
+
+const urlPadrao = "https://guimrl.github.io/mini-projetos/";
 const section = document.querySelector('#conteudo');
 
 projetos.forEach(projeto => {
     const tag = document.createElement("a");
-    tag.href = `${URL_PADRAO}${projeto.url}`;
+    tag.href = `${urlPadrao}${projeto.url}`;
     tag.classList.add("btn");
     tag.textContent = projeto.title;
     tag.target = "_blank";
