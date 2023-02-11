fb95b7a70808119d5a1e93968cd946ff3eb53429
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-11 16:42:41 -0300

    final commit

--- relogio/js/main.js
+++ relogio/js/main.js
@@ -3,21 +3,12 @@ let relogio = () => {
     let hrs = data.getHours();
     let min = data.getMinutes();
     let sec = data.getSeconds();
-    let periodo = "AM";
-    if (hrs == 0) {
-        hrs = 12;
-    }
-
-    if (hrs > 12) {
-        hrs = hrs - 12;
-        periodo = "PM";
-    }
 
     hrs = hrs < 10 ? "0" + hrs : hrs;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
 
-    let tempo = `${hrs}:${min}:${sec}:${periodo}`;
+    let tempo = `${hrs}:${min}:${sec}`;
     document.getElementById("conteudo").innerText = tempo;
     setTimeout(relogio, 1000);
 }
