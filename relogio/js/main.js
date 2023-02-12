04f8e63dabb6c1b2016280f97d68a828ec08b24b
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-12 12:55:35 -0300

    final commit

--- relogio/js/main.js
+++ relogio/js/main.js
@@ -1,16 +1,46 @@
+const diasDaSemana = {
+    0: "Dom",
+    1: "Seg",
+    2: "Ter",
+    3: "Qua",
+    4: "Qui",
+    5: "Sex",
+    6: "Sáb"
+};
+
+const mesesDoAno = {
+    0: "Janeiro",
+    1: "Fevereiro",
+    2: "Março",
+    3: "Abril",
+    4: "Maio",
+    5: "Junho",
+    6: "Julho",
+    7: "Agosto",
+    8: "Setembro",
+    9: "Outubro",
+    10: "Novembro",
+    11: "Dezembro"
+};
+
 let relogio = () => {
     let data = new Date();
     let hrs = data.getHours();
     let min = data.getMinutes();
     let sec = data.getSeconds();
+    let hoje = data.getDay();
+    let dia = data.getDate();
+    let mes = data.getMonth();
 
     hrs = hrs < 10 ? "0" + hrs : hrs;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
 
-    let tempo = `${hrs}:${min}:${sec}`;
-    document.getElementById("conteudo").innerText = tempo;
+    let horario = `${hrs}:${min}:${sec}`;
+    document.getElementById("relogio").innerText = horario;
     setTimeout(relogio, 1000);
+    let diaDeHoje = `${diasDaSemana[hoje]}, ${dia} de ${mesesDoAno[mes]}`;
+    document.getElementById("calendario").innerText = diaDeHoje;
 }
 
 relogio();
