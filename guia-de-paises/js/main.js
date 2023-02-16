8f44b46b038d31c58bec65e6dcf5ecd31f2a8302
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-16 18:26:09 -0300

    criando html e css

--- guia-de-paises/js/main.js
+++ guia-de-paises/js/main.js
@@ -1,1 +1,8 @@
-console.log('hello word');
+let pesquisar = document.getElementById("pesquisar");
+let input = document.getElementById("input-pais");
+
+pesquisar.addEventListener("click", () => {
+    let nomePais = input.value;
+    let url = `https://restcountries.com/v3.1/name/${nomePais}?fullText=true`;
+    console.log(url);
+});
