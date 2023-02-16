0c7b14e43d583920784b5f441414877996dd5302
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-16 18:47:15 -0300

    criando a bandeira dos paises

--- guia-de-paises/js/main.js
+++ guia-de-paises/js/main.js
@@ -5,4 +5,14 @@ pesquisar.addEventListener("click", () => {
     let nomePais = input.value;
     let url = `https://restcountries.com/v3.1/name/${nomePais}?fullText=true`;
     console.log(url);
+    fetch(url)
+        .then((res) => res.json())
+            .then((pais) => {
+            console.log(pais[0]);
+            // console.log(pais[0].capital[0]);
+            // console.log(pais[0].currencies[Object.keys(pais[0].currencies)].name)
+            resultado.innerHTML = `
+            <img src="${pais[0].flags.svg}" class="bandeira">
+            `;
+    });
 });
