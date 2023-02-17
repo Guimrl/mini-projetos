8c14ffa037eb43ab1f8f4bdda2ab1f2e92c91969
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-17 15:38:01 -0300

    criando layout principal

--- guia-de-paises/js/main.js
+++ guia-de-paises/js/main.js
@@ -3,14 +3,25 @@ let input = document.getElementById("input-pais");
 
 pesquisar.addEventListener("click", () => {
     let nomePais = input.value;
-    let url = `https://restcountries.com/v3.1/name/${nomePais}?fullText=true`;
+    // let url = `https://restcountries.com/v3.1/name/${nomePais}?fullText=true`;
+    let url =`https://restcountries.com/v3.1/translation/${nomePais}`;
     console.log(url);
     fetch(url)
         .then((res) => res.json())
             .then((pais) => {
-            console.log(pais[0]);
+            console.log(pais[0].translations.por.common);
+            // console.log(pais[0].flags.svg)
+            console.log(pais[0].name.common)
             // console.log(pais[0].capital[0]);
-            // console.log(pais[0].currencies[Object.keys(pais[0].currencies)].name)
+            // console.log(pais[0].continents[0])
+            // console.log(pais[0].population)
+            // console.log(pais[0].currencies[Object.keys(pais[0].currencies)].symbol)
+            // console.log(Object.keys(pais[0].currencies)[0])
+            // console.log(Object.values(pais[0].languages)
+            // .toString()
+            // .split(",")
+            // .join(", "))
+
             resultado.innerHTML = `
             <img src="${pais[0].flags.svg}" class="bandeira">
             `;
