54e7d25a7053c01534cc7a44fd4d2eb697fe07a1
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-17 16:32:36 -0300

    final commit

--- guia-de-paises/js/main.js
+++ guia-de-paises/js/main.js
@@ -1,30 +1,37 @@
 let pesquisar = document.getElementById("pesquisar");
 let input = document.getElementById("input-pais");
-const body = document.getElementsByTagName('body');
 
 pesquisar.addEventListener("click", () => {
     let nomePais = input.value;
-    // let url = `https://restcountries.com/v3.1/name/${nomePais}?fullText=true`;
-    let url =`https://restcountries.com/v3.1/translation/${nomePais}`;
-    // console.log(url);
+    let url = `https://restcountries.com/v3.1/translation/${nomePais}`;
+
     fetch(url)
-        .then((res) => res.json())
-            .then((pais) => {
-            console.log(pais[0].translations.por.common);
-            // console.log(pais[0].flags.svg)
-            console.log(pais[0].name.common)
-            // console.log(pais[0].capital[0]);
-            // console.log(pais[0].continents[0])
-            // console.log(pais[0].population)
-            // console.log(pais[0].currencies[Object.keys(pais[0].currencies)].symbol)
-            // console.log(Object.keys(pais[0].currencies)[0])
-            // console.log(Object.values(pais[0].languages)
-            // .toString()
-            // .split(",")
-            // .join(", "))
-            
+    .then((res) => res.json())
+        .then((pais) => {
             resultado.innerHTML = `
-            <img src="${pais[0].flags.svg}" class="bandeira">
+            <img src="${pais[0].flags.svg}" alt="${pais[0].flags.alt}" class="bandeira">
+            <h2>${pais[0].name.common} (${pais[0].translations.por.common})</h2>
+            <h4>Capital:</h4>
+            <span>${pais[0].capital[0]}</span><br>
+            <h4>Continent:</h4>
+            <span>${pais[0].continents[0]}</span><br>
+            <h4>Population:</h4>
+            <span>${pais[0].population}</span><br>
+            <h4>Currency:</h4>
+            <span>${pais[0].currencies[Object.keys(pais[0].currencies)].name} - ${Object.keys(pais[0].currencies)[0]} - ${pais[0].currencies[Object.keys(pais[0].currencies)].symbol}</span><br>
+            
+            <h4>languages:</h4>
+            <span>${Object.values(pais[0].languages)
+                .toString()
+                .split(",")
+                .join(", ")}
+            </span>
             `;
-    });
+        }).catch(() => {
+            if (nomePais.lenght == 0) {
+                resultado.innerHTML = `<h3>O campo não pode ser vazio</h3>`;
+            } else {
+                resultado.innerHTML = `<h3>Por favor insira um país válido<h3>`;
+            }
+        });
 });
