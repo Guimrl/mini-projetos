89fc361e3793bde17907298bc81593c5e9137ea2
Guilherme Amaral <gui.amaral123@outlook.com>
2023-05-09 23:10:25 +0000

    created: num and uppercase

--- gerador-senha/js/main.js
+++ gerador-senha/js/main.js
@@ -1,8 +1,21 @@
 
 function generatePassword() {
     let chars = "abcdef";
+    let nums = "1234";
     let length = 8;
     let password = "";
+    let num = false;
+    let upper = false;
+
+    if (num = true) {
+        chars = chars + nums;
+        console.log(chars)
+    }
+    
+    if (upper = true) {
+        chars = chars + chars.toUpperCase();
+        console.log(chars)
+    }
 
     for (let i = 0; i < length; i++) {
         let random = Math.floor(Math.random() * chars.length);
