0b3c11a2fc388607e722682ddb0dc881ce3cb5f6
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-05-14 23:29:27 -0300

    final commit

--- gerador-cor/js/main.js
+++ gerador-cor/js/main.js
@@ -1,17 +1,27 @@
 let btn = document.querySelector("#btn");
+let display = document.querySelector("#display");
+let canvas = document.querySelector("#canvas");
+let small = document.querySelector("small");
 let color;
 
 let random = () => {
-    color = '#';
-    let hex = '0123456789ABCDEF';
+    color = "#";
+    let hex = "0123456789ABCDEF";
     for (let i = 0; i < 6; i++) {
-        color = color + hex[Math.floor(Math.random() * 16)];
+        color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
 btn.addEventListener("click", () => {
-    document.body.style.backgroundColor = random();
-
+    canvas.style.backgroundColor = random();
+    display.innerHTML = color;
+    small.innerHTML = "Clique sobre a cor para copiá-la!";
 });
 
+[canvas, display].forEach((el) => {
+    el.addEventListener("click", () => {
+        navigator.clipboard.writeText(display.textContent);
+        small.innerHTML = "Cor copiada!";
+    });
+});
